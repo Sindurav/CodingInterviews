b5a3ef5e97193b7058bbd00bbf8aa034dea223f8
Jai Shah <jai.s@somaiya.edu>
2018-10-16 00:40:42 -0500

    first commit

--- TwoSigma 2019 Software Engineer Investments/friendCircle.py
+++ TwoSigma 2019 Software Engineer Investments/friendCircle.py
@@ -4,12 +4,12 @@ def friendCircles(friends):
     sets = []
     for i in range(0, len(friends)):
         sets.append(i)
-    
+
     for i in range(1, len(friends)):
         for j in range(0, i):
             if (friends[i][j]) == "Y":
                 sets[make_sets(i, sets)] = make_sets(j, sets)
-                
+
     res = set([])
     for i in range(0, len(friends)):
         res.add(make_sets(i, sets))
