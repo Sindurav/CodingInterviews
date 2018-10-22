aa78b9b92b07c16fc3a94dfbc662a81f7aa0cd63
Jai Shah <jai.s@somaiya.edu>
2018-10-22 00:08:27 -0500

    Coursera Folder Added

---
+++ Coursera Software Egnineer Entry Level - 2019/palindrome.py
@@ -0,0 +1,11 @@
+def palindrome(s):
+    n = len(s)
+    ans = set([])
+    for center in range(2*n - 1):
+        left = center / 2
+        right = left + center % 2
+        while left >= 0 and right < N and s[left] == s[right]:
+            ans.add(s[left: right+1])
+            left -= 1
+            right += 1
+    return len(ans)
