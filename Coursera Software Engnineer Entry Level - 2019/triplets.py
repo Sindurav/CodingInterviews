3d35f7ea2ebe07c697d568394d28a14c45faedac
Jai Shah <jai.s@somaiya.edu>
2018-10-22 12:16:55 -0500

    Coursera triplets solution added with O(n^2)

--- Coursera Software Engnineer Entry Level - 2019/triplets.py
+++ Coursera Software Engnineer Entry Level - 2019/triplets.py
@@ -8,3 +8,19 @@ def triplets(arr, t):
                 if k != j and k != i and arr[i] + arr[j] + arr[k] <= t:
                     count += 1
     return count
+
+
+def tripletsSorting(arr, t):
+    count = 0
+    arr = sorted(arr)
+    results = []
+    for i in range(len(arr)):
+        j, k = i + 1, len(arr) - 1
+        while j < k:
+            currentSum = arr[i] + arr[j] + arr[k]
+            if currentSum <= t:
+                results.append([arr[i], arr[j], arr[k]])
+                count += 1
+            k -= 1
+    print(results)
+    return count
