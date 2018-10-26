36e2bed1b5fcb66f4c9721d50446f84e5298dcaa
Jai Shah <jai.s@somaiya.edu>
2018-10-26 16:56:24 -0500

    Coursera Hackerrank Part 2 added images to examples in README

--- Coursera Software Engineer Entry Level-2019-Part-1/maxLength.py
+++ Coursera Software Engineer Entry Level-2019-Part-1/maxLength.py
@@ -7,14 +7,15 @@
 #  2. INTEGER k
 #
 
+
 def maxLength(a, k):
     # Write your code here
     summ = count = max_count = 0
     for i in range(len(a)):
-        if k>=summ+a[i]:
+        if k >= summ+a[i]:
             summ += a[i]
             count += 1
         elif summ != 0:
             summ = summ - a[i-count] + a[i]
-        if max_count<count: max_count = count
+        max_count = max(count, max_count)
     return max_count
