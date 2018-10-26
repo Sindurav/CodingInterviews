9541615d9082940cd3f0c52f462ee25dfb08475c
Jai Shah <jai.s@somaiya.edu>
2018-10-26 15:17:27 -0500

    Coursera triplets with sorting corrected

--- Coursera Software Engnineer Entry Level - 2019/triplets.py
+++ Coursera Software Engnineer Entry Level - 2019/triplets.py
@@ -1,16 +1,51 @@
+def bin_search(arr, target, start):
+    left, ryt = start, len(arr)-1
+    while left<len(arr) and ryt>= start and left<=ryt:
+        mid = (left+ryt)//2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid]>target:
+            ryt = mid-1
+        else:
+            left = mid+1
+    return left
+
+
+def tripletsSorting(nums, t):
+    nums.sort()
+    results = []
+    count = 0
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            target = t - nums[i] - nums[j]
+            if target<0:
+                break
+            k = bin_search(arr, target, j+1)
+            if k<len(nums) and nums[k]<=target:
+                count += (k-j)
+            elif k<len(nums):
+                count += (k-j-1)
+    print(count)
+    return count
+
 def triplets(arr, t):
     count = 0
+    sets = set([])
     for i in range(len(arr)):
         for j in range(len(arr)):
             if j == i:
                 continue
             for k in range(len(arr)):
-                if k != j and k != i and arr[i] + arr[j] + arr[k] <= t:
+                if k == j or k == i or i == j:
+                    continue
+                summ = (arr[i] + arr[j] + arr[k])
+                if frozenset((arr[i], arr[j], arr[k])) not in sets and summ <= t:
+                    sets.add(frozenset((arr[i], arr[j], arr[k])))
                     count += 1
+    print(count)
     return count
 
-
-def tripletsSorting(arr, t):
+def tripletsSortWithPruning(arr, t):
     count = 0
     arr = sorted(arr)
     results = []
@@ -27,5 +62,29 @@ def tripletsSorting(arr, t):
                     count += 1
                 else:
                     break
-    print("results =", results)
+    #print("results =", results)
+    print(count)
     return count
+
+
+arr = [1, 2, 3, 4, 5]
+threshold = 8
+tripletsSorting(arr, threshold)
+triplets(arr, threshold)
+tripletsSortWithPruning(arr, threshold)
+print("============")
+
+arr = [1, 2, 3, 4, 6, 9]
+threshold = 11
+tripletsSorting(arr, threshold)
+triplets(arr, threshold)
+tripletsSortWithPruning(arr, threshold)
+print("============")
+
+
+arr = [1, 2, 3, 4, 6]
+threshold = 11
+tripletsSorting(arr, threshold)
+triplets(arr, threshold)
+tripletsSortWithPruning(arr, threshold)
+print("============")
