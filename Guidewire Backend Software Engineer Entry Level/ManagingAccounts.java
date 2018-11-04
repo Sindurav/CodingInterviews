52207acaf034f88c2de25b05569a7af010081b95
Jai Shah <jai.s@somaiya.edu>
2018-11-04 01:17:43 -0500

    Guidewire codility test added

--- Guidewire Backend Software Engineer Entry Level/ManagingAccounts.java
+++ Guidewire Backend Software Engineer Entry Level/ManagingAccounts.java
@@ -3,16 +3,15 @@ class ManagingAccoutns
     public int solution(int A) 
     {
         // write your code in Java SE 8
-        
+
+        String num;
         num = String.valueOf(A);
         
         if (num.length() == 1)
         {
             return A;
         }
-        
 		
-        String num;
         int i, j;
         String res;        
         
@@ -22,12 +21,9 @@ class ManagingAccoutns
         
         while (i<j)
         {
-            //System.out.println(num.charAt(i));
-            //System.out.println(num.charAt(j));
             res += String.valueOf(num.charAt(i))+String.valueOf(num.charAt(j));
             i = i+1;
             j = j-1;
-            
         }
         
         if (i == j)
