23361c2c32fd411ca7eca1aa30a8cb355700a73f
Jai Shah <jai.s@somaiya.edu>
2018-11-14 14:16:36 -0600

    corrected the compilation errors

--- HARMAN International Software Engineer 2019/codeDependency.py
+++ HARMAN International Software Engineer 2019/codeDependency.py
@@ -1,11 +1,12 @@
 def dependency_check(code_id):
+    seen = set([])
     visited = set([])
     dependency_list = []
-    topological_sort(code_id, dependency_list)
+    topological_sort(code_id, dependency_list, visited, seen)
     return dependency_list
 
 
-def topological_sort(parent_code_id):
+def topological_sort(parent_code_id, dependency_list, visited, seen):
     if code_id in visited:
         return
 
@@ -15,7 +16,7 @@ def topological_sort(parent_code_id):
     for child_code_id in parent_code_id.children:
         if child_code_id in seen:
             print("Cyclic dependency present")
-        topological_sort(code_id)
+        topological_sort(code_id, dependency_list, visited, seen)
 
     seen.remove(parent_code_id)
     dependency_list.append(parent_code_id)
