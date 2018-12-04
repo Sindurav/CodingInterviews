158ef431b01a2d391d81e0969aed28f2ef85a8e5
Jai Shah <jai.s@somaiya.edu>
2018-12-04 13:04:17 -0600

    Amazon SDE added

---
+++ Amazon SDE - 1/kNearestPoints.py
@@ -0,0 +1,24 @@
+import math
+
+
+def compute_euclidean_distance(x1, y1, x2, y2):
+    return math.sqrt(((x1-x2)**2)+((y1-y2)**2))
+
+
+def nearest_neighbor(points, goal_point, k):
+    distances = []
+
+    for point in points:
+        distance = compute_euclidean_distance(point[0], point[1], goal_point[0], goal_point[1])
+        distances.append([point, distance])
+    else:
+        distances.sort(key=lambda element: element[-1])
+
+    closest_neighbors = [distances[i][0] for i in range(0, k)]
+    return closest_neighbors
+
+
+points_array = [[6, 3], [2, 1], [5, 2], [3, 2], [9, 0]]
+goal = [3, 2]
+neighbors = nearest_neighbor(points_array, goal, 3)
+print(neighbors)
---
+++ Amazon SDE - 1/shortestPath.py
@@ -0,0 +1,50 @@
+from collections import deque
+
+
+def is_valid(x, y, n, m, matrix, visited):
+    if (0 <= x < n) and (0 <= y < m) and (matrix[x][y] == 1) and ((x, y) not in visited):
+        visited.add((x, y))
+        return True
+    return False
+
+
+def shortest_path(matrix, start, goal):
+    neighbors = ((0, 1), (0, -1), (1, 0), (-1, 0))
+
+    visited = set([])
+    visited.add((start[0], start[1]))
+
+    queue = deque()
+    queue.appendleft([start[0], start[1], 0, [start]])
+
+    while queue:
+
+        x, y, step, path = queue.pop()
+
+        if [x, y] == goal:
+            return path
+
+        for i in range(len(neighbors)):
+            new_x = x+neighbors[i][0]
+            new_y = y+neighbors[i][1]
+            if is_valid(new_x, new_y, len(matrix), len(matrix[0]), matrix, visited):
+                queue.appendleft((new_x, new_y, step+1, path+[[new_x, new_y]]))
+
+    return []
+
+
+mat = [[1, 0, 1, 1, 1, 1, 0, 1, 1, 1],
+       [1, 0, 1, 0, 1, 1, 1, 0, 1, 1],
+       [1, 1, 1, 0, 1, 1, 0, 1, 0, 1],
+       [1, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+       [1, 1, 1, 0, 1, 1, 1, 1, 1, 0],
+       [1, 0, 1, 1, 1, 1, 0, 1, 0, 0],
+       [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+       [1, 0, 1, 1, 1, 1, 0, 1, 1, 1],
+       [1, 1, 0, 0, 0, 0, 1, 0, 0, 1]]
+
+
+a = [0, 0]
+b = [5, 7]
+result_path = shortest_path(mat, a, b)
+print(result_path)
